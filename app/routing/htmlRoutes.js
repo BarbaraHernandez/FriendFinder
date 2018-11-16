85f019d40be95a622eea5b92a5e409d327d92313
BarbaraHernandez <bandezanne@gmail.com>
2018-11-16 01:50:46 -0500

    moved logic to html script tag

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -12,8 +12,8 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
-    //default GET home.html
-    app.get(function (req, res) {
+    //GET home.html
+    app.get("/home", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };

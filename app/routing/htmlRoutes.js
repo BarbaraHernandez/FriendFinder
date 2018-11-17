32deb0e15dff2352e4429d2b45995141d2496ad8
BarbaraHernandez <bandezanne@gmail.com>
2018-11-17 09:46:17 -0500

    updated routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -13,7 +13,7 @@ module.exports = function(app) {
     });
 
     //GET home.html
-    app.get("/home", function (req, res) {
+    app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
